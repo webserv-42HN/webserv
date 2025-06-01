2c39ba7e20079f1b127b1e68e492357f848feb33
oemelyan <oemelyan@student.42.fr>
2025-06-01 09:46:22 +0200

    code cleanup

--- src/Server.cpp
+++ src/Server.cpp
@@ -31,7 +31,6 @@ void Server::run() {
 
 void Server::signalHandler(int signum) {
 	(void)signum;
-	// std::cout << "Shuttimg down the server..." << std::endl;
 	running = false;
 }
 
@@ -40,7 +39,6 @@ void stopLoop(int) {
 }
 
 void Server::mainLoop() {
-	// std::cout << "DEBUG: main LOOp" << std::endl;
 	while (running) {
 		int poll_count = poll(poll_fds.data(), poll_fds.size(), 1000);
 		if (poll_count < 0) {
@@ -67,7 +65,6 @@ void Server::mainLoop() {
 }
 
 void Server::handleNewConnection(int listen_id) {
-	std::cout << "DEBUG: handleNewConnection" << std::endl;
 	int client_fd = accept(listen_id, nullptr, nullptr);
 		if (client_fd < 0) {
 			perror("accept");
@@ -79,8 +76,6 @@ void Server::handleNewConnection(int listen_id) {
 }
 
 void Server::handleClientData(int client_fd) {
-
-	std::cout << "DEBUG: handleClientData" << std::endl;
 	char buf[BUF_SIZE];
 	ssize_t nread = recv(client_fd, buf, BUF_SIZE - 1, 0);
 	if (nread <= 0) {
@@ -100,7 +95,6 @@ void Server::handleClientData(int client_fd) {
 		session.content_length = res.getContentLength(headers);
 	}
 
-	// If full request (headers + body) received
 	if (session.headers_received && session.buffer.size() >= header_end + session.content_length) {
 		Response res;
 		std::string full_request = session.buffer;
@@ -113,9 +107,8 @@ void Server::handleClientData(int client_fd) {
 			response = res.routing(res.getRequestLine().method, res.getRequestLine().url);
 		}
 		responses[client_fd] = response;
-		client_sessions.erase(client_fd); // Clear session
+		client_sessions.erase(client_fd);
 
-		// Enable writing
 		for (auto& pfd : poll_fds) {
 			if (pfd.fd == client_fd) {
 				pfd.events = POLLOUT;
@@ -127,7 +120,6 @@ void Server::handleClientData(int client_fd) {
 }
 
 void Server::handleClientWrite(int client_fd) {
-	std::cout << "DEBUG: handleClientWrite" << std::endl;
 	auto it = responses.find(client_fd);
 	if (it == responses.end()) {
 		std::cerr << "No response found for client " << client_fd << std::endl;
@@ -142,7 +134,6 @@ void Server::handleClientWrite(int client_fd) {
 		responses.erase(client_fd);
 		return ;
 	}
-	// std::cout << "Sent response to client :\n" << response << std::endl;
 
 	responses.erase(client_fd);
 
@@ -152,7 +143,6 @@ void Server::handleClientWrite(int client_fd) {
 			break;
 		}
 	}
-	//closeClient(client_fd);
 }
 
 void Server::closeClient(int client_fd){
@@ -164,7 +154,6 @@ void Server::closeClient(int client_fd){
 			break;
 		}
 	}
-
 	clientConfigs.erase(client_fd);
 }
 
@@ -226,62 +215,10 @@ void Server::setupPorts() {
 			poll_fds.push_back(pollfd);
 			ss_Fds.push_back(it->sock_fd);
 			uniqPorts.push_back(it->port);
-			serverSockets[it->sock_fd] = *it; //I added the server socket to map
-			std::cout << "we pushed: "  << "sock_fd: " << it->sock_fd << ", port: " << it->port << std::endl;
+			serverSockets[it->sock_fd] = *it;
+			//std::cout << "we pushed: "  << "sock_fd: " << it->sock_fd << ", port: " << it->port << std::endl;
 		}
 		++it;
 	}
-
-//======================check of setup==============================
-//======================to delelte==================================
-
-	std::cout << "=== Results of setupPorts() ===" << std::endl;
-std::cout << "somaxconn: " << SOMAXCONN << std::endl;
-// Print unique ports
-std::cout << "[Unique Ports]" << std::endl;
-for (std::vector<int>::iterator it = uniqPorts.begin(); it != uniqPorts.end(); ++it) {
-	std::cout << "Port: " << *it << std::endl;
 }
 
-// Print server socket file descriptors
-std::cout << "\n[Server Socket FDs]" << std::endl;
-for (std::vector<int>::iterator it = ss_Fds.begin(); it != ss_Fds.end(); ++it) {
-	std::cout << "Socket FD: " << *it << std::endl;
-}
-
-// Print poll file descriptors
-std::cout << "\n[Poll FDs]" << std::endl;
-for (std::vector<struct pollfd>::iterator it = poll_fds.begin(); it != poll_fds.end(); ++it) {
-	std::cout << "pollfd { fd: " << it->fd
-	          << ", events: " << it->events
-	          << ", revents: " << it->revents << " }" << std::endl;
-}
-//======================check of setup==============================
-//======================to delelte==================================
-
-}
-
-
-
-
-
-
-//==============================================================
-//===========example of poll usage==============================
-
-// int ret = poll(fds, nfds, timeout);
-// if (ret == -1) {
-//     if (errno == EINTR) {
-//         // handle signal interruption or just retry
-//     } else {
-//         perror("poll failed");
-//         exit(EXIT_FAILURE);
-//     }
-// } else if (ret == 0) {
-//     // timeout, no events
-// } else {
-//     // check each fds[i].revents to see what happened
-// }
-
-//--------------------------------------------------------------
-//--------------------------------------------------------------
